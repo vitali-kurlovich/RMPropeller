463dd965eadcc98b74cdda4e7ea03701dcbbeac8
vitali-kurlovich <vitalikurlovich@MacBook-Pro-Vitali.local>
2016-04-29 19:58:14 +0300

    add empty test

---
+++ tests/engine/graphics/material/RMPassTest.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Vitali Kurlovich on 4/29/16.
+//
+
+#include "gtest/gtest.h"
+
+//#include <graphics/material/pass/RMPass.hpp>
+
+TEST(RMPass, emptyTest) {
+    int a = 5;
+    int b = 3;
+
+    EXPECT_EQ(a+b, 8);
+}
