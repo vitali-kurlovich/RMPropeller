b4d39a98ac52af7bf0ea8c117ea6b2e8e226b664
Vitali Kurlovich <vitalikurlovich@Vitalis-MacBook-Pro.local>
2016-08-16 15:14:29 +0300

    add glad

--- main.cpp
+++ main.cpp
@@ -2,11 +2,21 @@
 // Created by Vitali Kurlovich on 4/21/16.
 //
 
-
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <OpenGL/gl.h>
+
+
+// GLAD_DEBUG is only defined if the c-debug generator was used
+#ifdef GLAD_DEBUG
+// logs every gl call to the console
+void pre_gl_call(const char *name, void *funcptr, int len_args, ...) {
+    printf("Calling: %s (%d arguments)\n", name, len_args);
+}
+#endif
+
+
 
 static void error_callback(int error, const char* description)
 {
@@ -23,15 +33,58 @@ int main(void)
     glfwSetErrorCallback(error_callback);
     if (!glfwInit())
         exit(EXIT_FAILURE);
+
+#ifdef __APPLE__
+    // Select OpenGL 4.1
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 1 );
+#else
+    // Select OpenGL 4.3
+  glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
+  glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+#endif
+
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+
+
     window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
+
+
+
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
     glfwSetKeyCallback(window, key_callback);
+
+
+
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+
+    {
+       // std::cout << "Failed to initialize OpenGL context" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+#ifdef GLAD_DEBUG
+    // before every opengl call call pre_gl_call
+    glad_set_pre_callback(pre_gl_call);
+
+    // post callback checks for glGetError by default
+
+    // don't use the callback for glClear
+    // (glClear could be replaced with your own function)
+    glad_debug_glClear = glad_glClear;
+#endif
+
+
     while (!glfwWindowShouldClose(window))
     {
         float ratio;
@@ -39,21 +92,11 @@ int main(void)
         glfwGetFramebufferSize(window, &width, &height);
         ratio = width / (float) height;
         glViewport(0, 0, width, height);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.6f, 0.f);
-        glEnd();
+
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
